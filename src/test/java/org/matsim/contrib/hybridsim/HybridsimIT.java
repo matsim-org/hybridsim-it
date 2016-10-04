68d641ab9275f54d77c2ea38d20fdd86ba1ff812
Michael Zilske <michael.zilske@tu-berlin.de>
2016-10-04 12:28:58 +0200

    First try.

---
+++ src/test/java/org/matsim/contrib/hybridsim/HybridsimIT.java
@@ -0,0 +1,15 @@
+package org.matsim.contrib.hybridsim;
+
+import org.junit.Test;
+import org.matsim.contrib.hybridsim.run.RunExample;
+
+import java.io.IOException;
+
+public class HybridsimIT {
+
+	@Test
+	public void testHybridsim() throws IOException, InterruptedException {
+		RunExample.main(new String[]{});
+	}
+
+}
